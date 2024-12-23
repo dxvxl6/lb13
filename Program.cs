dadaa0e64892022a885872d8fc9c533479fd0490
dxvxl6 <147540545+dxvxl6@users.noreply.github.com>
2024-12-23 17:30:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace lb13
+{
+    /// <summary>
+    /// Этот класс представлен в виде простого калькулятора.
+    /// </summary>
+    public class Calculator
+    {
+        /// <summary>
+        /// Вычисляет сумму двух целых чисел.
+        /// </summary>
+        /// <param name="a">Первое число для сложения.</param>
+        /// <param name="b">Второе число для сложения.</param>
+        /// <returns>Сумма параметров a и b.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Генерируется, если одно из чисел меньше нуля.
+        /// </exception>
+        public int Sum(int a, int b)
+        {
+            if (a < 0 || b < 0)
+                throw new ArgumentOutOfRangeException("Числа не должны быть отрицательными");
+
+            return a + b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine(calculator.Sum(1, 2));
+        }
+    }
+}
